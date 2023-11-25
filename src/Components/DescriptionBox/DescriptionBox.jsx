a0c533ff6f89a3ab69ed5edc848bd93f7fc62f4f
darshanvaghani0 <darshanvaghani0@gmail.com>
2023-11-25 11:04:11 +0530

    New Project Created

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+    return (
+        <div className='descriptionbox'>
+            <div className="descriptionbox-navigator">
+                <div className="descriptionbox-nav-box">Description</div>
+                <div className="descriptionbox-nav-box fade">Review(12)</div>
+            </div>
+            <div className="descriptionbox-description">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla quibusdam nobis cumque incidunt molestiae! Facilis eaque nemo neque sint ratione quos autem. Laudantium rem suscipit ullam aut iure repellendus dolorum.Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla quibusdam nobis cumque incidunt molestiae! Facilis eaque nemo neque sint ratione quos autem. Laudantium rem suscipit ullam aut iure repellendus dolorum.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla quibusdam nobis cumque incidunt molestiae! Facilis eaque nemo neque sint ratione quos autem. Laudantium rem suscipit ullam aut iure repellendus dolorum.Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla quibusdam nobis cumque incidunt molestiae! Facilis eaque nemo neque sint ratione quos autem. Laudantium rem suscipit ullam aut iure repellendus dolorum.</p>
+            </div>
+        </div>
+    )
+}
+
+export default DescriptionBox
